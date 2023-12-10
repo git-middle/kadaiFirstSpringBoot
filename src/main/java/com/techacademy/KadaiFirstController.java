426cdfd7d1988a2c72c74c5549d99bdcbd85e74d
iikot <iikotoarusa11@gmail.com>
2023-12-10 15:24:35 +0900

    符号修正

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -31,21 +31,21 @@ public class KadaiFirstController {
     @GetMapping("/minus/{val1}/{val2}")
     public String calcMinus(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 - val2;
         return "実行結果：" + res;
     }
 
     @GetMapping("/times/{val1}/{val2}")
     public String calcTimes(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 * val2;
         return "実行結果：" + res;
     }
 
     @GetMapping("/divide/{val1}/{val2}")
     public String calcDivide(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 / val2;
         return "実行結果：" + res;
     }
 
